349eb1d4f2318f7546107fd65150045fffc612c9
Shahroz002 <94517545+Rizwan1230786@users.noreply.github.com>
2022-05-16 15:40:27 +0500

    scripts file add

--- resources/views/userside/layouts/script.blade.php
+++ resources/views/userside/layouts/script.blade.php
@@ -1,20 +1,20 @@
-<script src="../../../app-assets/vendors/js/vendors.min.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/vendors/js/vendors.min.js"></script>
 <!-- BEGIN Vendor JS-->
 
 <!-- BEGIN: Page Vendor JS-->
-<script src="../../../app-assets/vendors/js/ui/jquery.sticky.js"></script>
-<script src="../../../app-assets/vendors/js/charts/apexcharts.min.js"></script>
-<script src="../../../app-assets/vendors/js/extensions/toastr.min.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/vendors/js/ui/jquery.sticky.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/vendors/js/charts/apexcharts.min.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/vendors/js/extensions/toastr.min.js"></script>
 <!-- END: Page Vendor JS-->
 
 <!-- BEGIN: Theme JS-->
-<script src="../../../app-assets/js/core/app-menu.min.js"></script>
-<script src="../../../app-assets/js/core/app.min.js"></script>
-<script src="../../../app-assets/js/scripts/customizer.min.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/js/core/app-menu.min.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/js/core/app.min.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/js/scripts/customizer.min.js"></script>
 <!-- END: Theme JS-->
 
 <!-- BEGIN: Page JS-->
-<script src="../../../app-assets/js/scripts/pages/dashboard-ecommerce.min.js"></script>
+<script src="{{ asset('/userside') }}/app-assets/js/scripts/pages/dashboard-ecommerce.min.js"></script>
 <!-- END: Page JS-->
 
 <script>
