a6a32bdb5337d2fef3c6d9af38a407912c2fcd04
Jahanzaib-hwryk75 <jahanzaibshakeel@hwryk.com>
2022-05-18 16:33:01 +0500

    hover issue fixed

--- resources/views/userside/layouts/navbar.blade.php
+++ resources/views/userside/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <div id="header">
 	<div id="headerrightcorner" style="margin-right:5px;">
-		<div id="header_body" >
+		<div id="header_body">
 			<ul>
 				<li class="z-logo-icon-header">
 					<a href="https://www.abadkar.com/" rel="nofollow" id="header_home">Go to abadkar.com</a>
@@ -8,44 +8,48 @@
 				<li class="z-email-icon-header">{{ Auth::guard('customeruser')->user()->email }}</li>
 				<li class="z-logout-icon-header">
 					<a href="{{asset('front')}}/profolio/logout.php" id="" rel="nofollow">
-						Logout					</a>
+						Logout </a>
 				</li>
 			</ul>
 		</div>
 	</div>
 
 	<a href="{{asset('userside')}}/profolio/">
-	<div id="headerleftcorner"><img src="{{ asset('front/images/abadkar-logo.png') }}" alt="" width="150" style="margin-top: 1px;"></div>
+		<div id="headerleftcorner"><img src="{{ asset('front/images/abadkar-logo.png') }}" alt="" width="150" style="margin-top: 1px;"></div>
 	</a>
 </div>
 
 <div id="mybayut_tabs">
-<li><a href='{{asset('userside')}}/profolio/index.php'>Dashboard</a></li><li id=current><a href='{{asset('userside')}}/profolio/index.php?tabs=2&section=listings&status=on'>Property Management</a></li><li><a href='{{asset('userside')}}/profolio/index.php?section=mailbox_inbox&tabs=3&type=u'>Message Center</a></li><li><a href='{{asset('userside')}}/profolio/index.php?tabs=4&section=editUser'>My Account & Profiles</a></li><li><a href='{{asset('userside')}}/profolio/index.php?tabs=10&section=reports'>Reports</a></li><li><a href='{{asset('userside')}}/profolio/index.php?tabs=6&section=favourites'>Tools</a></li><li><a href='{{asset('userside')}}/profolio/index.php?tabs=7&section=agedit_user'>Agency Staff</a></li><li><a href='{{asset('userside')}}/profolio/index.php?tabs=8&section=inquiries&subsection=lead_summary' class='client_n_leads'>Clients & Leads</a></li><li><a href='{{asset('userside')}}/profolio/index.php?tabs=12&section=agency_ws'>Agency Website</a></li><li><a href='{{asset('userside')}}/profolio/index.php?tabs=13&section=advertise'>Advertise</a></li><div class="clearfix"></div>
+	<li><a href='/user/dashboard'>Dashboard</a></li>
+	<li><a href='#'>Property Management</a></li>
+	<li><a href='#'>Message Center</a></li>
+	<li><a href='#'>My Account & Profiles</a></li>
+	<li><a href='#'>Reports</a></li>
+	<li><a href='#'>Tools</a></li>
+	<li><a href='#'>Agency Staff</a></li>
+	<li><a href='#' class='client_n_leads'>Clients & Leads</a></li>
+	<li><a href='#'>Agency Website</a></li>
+	<li><a href='#'>Advertise</a></li>
+	<div class="clearfix"></div>
 </div>
-
-
-		<div class="prop-nav left">
-			<div class="left">
-				<a href="#" class="checkout_cart menu_list cart_icon icon-utl transparent" onclick="return false;" >
-					cart
-								<span id="header_cart_count" style="
+<div class="prop-nav left">
+	<div class="left">
+		<a href="#" class="checkout_cart menu_list cart_icon icon-utl transparent" onclick="return false;">
+			cart
+			<span id="header_cart_count" style="
 						position: absolute;right: -5px;top: -5px;min-height: 8px;min-width: 10px;text-align: center;background: red;border-radius:50%;
 					    padding: 3px;color: #fff;font-size: 9px;line-height: 10px;display:none;
 					    ">0
-				</span>
-				</a>
-				<span id="new" style="display: none;">0</span>
-				<a href="{{asset('userside')}}/profolio/index.php?tabs=13&section=billing" rel="nofollow" class="menu_list transparent billing_icon icon-utl" id="header_billing">Billing</a>
-				<a href="{{asset('userside')}}/profolio/index.php?tabs=6&section=edit_advance_alerts" class="menu_list email_alert_icon icon-utl transparent">Email Alert</a>
-				<a href="https://www.zameen.com/blog/" rel="nofollow" id="header_blog" class="menu_list transparent blog_icon icon-utl">Blog</a>
-
-				<a href="https://www.zameen.com/advertise/index.html" rel="nofollow" id="header_advertise" class="menu_list transparent advertise-icon icon-utl">Advertise</a>
-			</div>
-			<div class="right">
-
-				<a href="{{asset('userside')}}/profolio/index.php?tabs=2&section=inventory_search" class="menu_list invent_search_icon icon-utl">Inventory Search</a>
-
-				<a href="index.php?tabs=2&section=add_property" target="_self" class="menu_list post_list_icon icon-utl">Post Listing</a>
-			</div>
-
-		</div>
+			</span>
+		</a>
+		<span id="new" style="display: none;">0</span>
+		<a href="{{asset('userside')}}/profolio/index.php?tabs=13&section=billing" rel="nofollow" class="menu_list transparent billing_icon icon-utl" id="header_billing">Billing</a>
+		<a href="{{asset('userside')}}/profolio/index.php?tabs=6&section=edit_advance_alerts" class="menu_list email_alert_icon icon-utl transparent">Email Alert</a>
+		<a href="https://www.zameen.com/blog/" rel="nofollow" id="header_blog" class="menu_list transparent blog_icon icon-utl">Blog</a>
+		<a href="https://www.zameen.com/advertise/index.html" rel="nofollow" id="header_advertise" class="menu_list transparent advertise-icon icon-utl">Advertise</a>
+	</div>
+	<div class="right">
+		<a href="{{asset('userside')}}/profolio/index.php?tabs=2&section=inventory_search" class="menu_list invent_search_icon icon-utl">Inventory Search</a>
+		<a href="index.php?tabs=2&section=add_property" target="_self" class="menu_list post_list_icon icon-utl">Post Listing</a>
+	</div>
+</div>
