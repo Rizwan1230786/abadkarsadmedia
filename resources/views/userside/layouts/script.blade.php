49dec9bb5a527bde153f4078b7d578717160e110
Jahanzaib-hwryk75 <jahanzaibshakeel@hwryk.com>
2022-05-18 11:21:54 +0500

    new settings

--- resources/views/userside/layouts/script.blade.php
+++ resources/views/userside/layouts/script.blade.php
@@ -18,9 +18,12 @@
 <!-- END: Page JS-->
 @yield('js')
 <script>
-  $(window).on('load',  function(){
+  $(window).on('load', function() {
     if (feather) {
-      feather.replace({ width: 14, height: 14 });
+      feather.replace({
+        width: 14,
+        height: 14
+      });
     }
   })
 </script>
