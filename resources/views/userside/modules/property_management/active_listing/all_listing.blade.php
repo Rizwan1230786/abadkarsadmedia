f6ca7bf10e4c8c6971cbe0f62feb6836a60e58b0
Shahroz002 <94517545+Rizwan1230786@users.noreply.github.com>
2022-05-20 16:58:43 +0500

    add rent listing table

--- resources/views/userside/modules/property_management/active_listing/all_listing.blade.php
+++ resources/views/userside/modules/property_management/active_listing/all_listing.blade.php
@@ -2,39 +2,81 @@
 @section('main')
     @include('userside.layouts.sidebar')
     <div id="rightcolumn" style="
-    width:79% " class="rightcolumn_div post_story_margin">
-    <span id="table_container" class="d-none">
-        <div class="box_title">
-            <div><b>Active Listing For Sale</b></div>
-        </div>
-        <div class="box_body listing-property-profolio" id="Sale_listings" style="padding: 0px; ">
-            <div class="ant-table" id="data_Sale" style="height:auto">
-
-                <table id="myTable" class="table table-responsive listing_table table-bordered text-nowrap">
-                    <thead class="thead-light" id="table_head">
-                        <tr>
-                            <th style="padding:0px 0px 0px 15px;">ID</th>
-                            <th>Type</th>
-                            <th>Location</th>
-                        </tr>
-                    </thead>
-                    <tbody id="table_data" style="float: none;">
-                        @isset($property)
-                        @foreach ($property as $value )
-                        @if(isset($value['type']) && $value['type'] == 'sale')
-                        <tr>
-                            <td>{{ $value->id }}</td>
-                            <td>{{ $value->category }}</td>
-                            <td>{{ $value->location }}</td>
-                        </tr>
-                        @endif
-                        @endforeach
-                        @endisset
-                    </tbody>
-                </table>
+        width:79% " class="rightcolumn_div post_story_margin">
+        <span id="table_container" class="d-none">
+            <div class="box_title">
+                <div><b>Active Listing For Sale</b></div>
             </div>
-        </div>
-    </span>
+            <div class="box_body listing-property-profolio" id="Sale_listings" style="padding: 0px; ">
+                <div class="ant-table" id="data_Sale" style="height:auto">
+
+                    <table id="myTable" class="table table-responsive listing_table table-bordered text-nowrap">
+                        <thead class="thead-light" id="table_head">
+                            <tr>
+                                <th style="padding:0px 0px 0px 15px;">ID</th>
+                                <th>Type</th>
+                                <th>Location</th>
+                            </tr>
+                        </thead>
+                        <tbody id="table_data" style="float: none;">
+                            @isset($property)
+                                @foreach ($property as $value)
+                                    @if (isset($value['type']) && $value['type'] == 'sale')
+                                        <tr>
+                                            <td>{{ $value->id }}</td>
+                                            <td>{{ $value->category }}</td>
+                                            <td>{{ $value->location }}</td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            @endisset
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </span>
+        <br>
+        <br>
+        <span id="table_container" class="d-none">
+            <div class="box_title">
+                <div><b>Active Listing For Rent</b></div>
+            </div>
+            <div class="box_body listing-property-profolio" id="Sale_listings" style="padding: 0px; ">
+                <div class="ant-table" id="data_Sale" style="height:auto">
+
+                    <table id="myTable2" class="table table-responsive listing_table table-bordered text-nowrap">
+                        <thead class="thead-light" id="table_head">
+                            <tr>
+                                <th style="padding:0px 0px 0px 15px;">ID</th>
+                                <th>Type</th>
+                                <th>Location</th>
+                            </tr>
+                        </thead>
+                        <tbody id="table_data" style="float: none;">
+                            @isset($property)
+                                @foreach ($property as $value)
+                                    @if (isset($value['type']) && $value['type'] == 'rent')
+                                        <tr>
+                                            <td>{{ $value->id }}</td>
+                                            <td>{{ $value->category }}</td>
+                                            <td>{{ $value->location }}</td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            @endisset
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </span>
     </div>
-</div>
+    </div>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+        $(document).ready(function() {
+            $('#myTable2').DataTable();
+        });
+    </script>
 @endsection
