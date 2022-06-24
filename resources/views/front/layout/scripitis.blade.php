1b481fd50fcb963a7fd2a904c6948b24c8b8dcf6
Shahroz002 <94517545+Rizwan1230786@users.noreply.github.com>
2022-06-24 16:43:25 +0500

    add top navbar

--- resources/views/front/layout/scripitis.blade.php
+++ resources/views/front/layout/scripitis.blade.php
@@ -54,7 +54,71 @@
   <script src="{{ asset('front') }}/js/dashbord-mobile-menu.js"></script>
   <script src="{{ asset('front') }}/js/forms-2.js"></script>
   <script src="{{ asset('front') }}/js/dropzone.js"></script>
+<script>
+    const marqueeArr = document.querySelectorAll('.marquee');
+marqueeArr.forEach(marquee => {
+  const marqueeRow = marquee.querySelector('.marquee__row');
+  const marqueeItem = marqueeRow.querySelector('.marquee__item');
+  const cloneNum = Number(marqueeItem.getAttributeNode('data-clone').value);
+  for (let i = 1; i < cloneNum; i++) {
+    const clone = marqueeItem.cloneNode(true);
+    marqueeRow.appendChild(clone);
+  }
+  for (let i = 0; i < 2; i++) {
+    const clone = marqueeRow.cloneNode(true);
+    marquee.appendChild(clone);
+  }
 
+  const marqueeMove = (dir) => {
+    const rows = marquee.querySelectorAll('.marquee__row');
+    rows.forEach(row => {
+      let rowWidth = row.getBoundingClientRect().width;
+      let currentX = Number(getComputedStyle(row).getPropertyValue('--pos-x'));
+      let newX = 0;
+      switch (dir) {
+        case 'left':
+          newX = currentX ? (currentX - 1) : -rowWidth;
+          (newX < (-2 * rowWidth)) && (newX = -rowWidth);
+          break;
+        default:
+          newX = currentX ? (currentX + 1) : -rowWidth;
+          (newX > 0) && (newX = -rowWidth);
+      }
+      row.style.setProperty('--pos-x', newX);
+    });
+  };
+
+
+  let speed = Number(marquee.getAttributeNode('data-speed').value);
+  let direction = 'left';
+  let marqueeInterval = setInterval(marqueeMove, speed, direction);
+  marquee.onmouseenter = () => {
+    clearInterval(marqueeInterval);
+  }
+  marquee.onmousemove = () => {
+    clearInterval(marqueeInterval);
+  }
+  marquee.onmouseleave = () => {
+    clearInterval(marqueeInterval);
+    marqueeInterval = setInterval(marqueeMove, speed, direction);
+  }
+
+  let posY = 0;
+  const changeDir = () => {
+    clearInterval(marqueeInterval);
+    let scrollTop = document.documentElement.scrollTop;
+    direction = (scrollTop > posY) ? 'right' : 'left';
+    marqueeMove(direction);
+    marqueeMove(direction);
+    marqueeInterval = setInterval(marqueeMove, speed, direction);
+    posY = scrollTop;
+  };
+  window.addEventListener('scroll', changeDir);
+
+});
+
+
+</script>
   <script>
       $(function() {
           $("#slider").slick({
