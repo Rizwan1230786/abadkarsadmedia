239a4ee36e1e0521084e28b5fd4fc6eed36bf805
Shahroz002 <94517545+Rizwan1230786@users.noreply.github.com>
2022-07-29 11:03:02 +0500

    cange password decript to hashmake

--- app/Http/Controllers/admin/realestate/AgencyController.php
+++ app/Http/Controllers/admin/realestate/AgencyController.php
@@ -68,7 +68,7 @@ class AgencyController extends Controller
                 $agencyportal = new AgencyPortal();
                 $agencyportal['email']=$agency->email;
                 $agencyportal['agency_id']=$agency->id;
-                $agencyportal['password']=encrypt($randum_pasword);
+                $agencyportal['password']=Hash::make($randum_pasword);
                 $agencyportal['type']="agency";
                 Mail::send('agency.modules.agent.mail.mails', ['email'=> $agencyportal['email'] , 'password' => $randum_pasword], function($message) use($request){
                     $message->to($request->email);
@@ -98,8 +98,9 @@ class AgencyController extends Controller
     }
 
     public function resend_email_agency(Request $request){
+
         $resend_data=AgencyPortal::where('agency_id',$request->id)->first();
-        $password=decrypt($resend_data->password);
+        $password=$resend_data->password;
         $email=$resend_data->email;
         Mail::send('agency.modules.agent.mail.mails', ['email'=> $email , 'password' => $password], function($message) use($email){
             $message->to($email);
--- app/Http/Controllers/agency/AgentsController.php
+++ app/Http/Controllers/agency/AgentsController.php
@@ -73,7 +73,7 @@ class AgentsController extends Controller
                 $agentportal = new AgencyPortal();
                 $agentportal['email'] = $agent->email;
                 $agentportal['agent_id']=$agent->id;
-                $agentportal['password'] = encrypt($randum_pasword);
+                $agencyportal['password']=Hash::make($randum_pasword);
                 $agentportal['type'] = "agent";
                 Mail::send('agency.modules.agent.mail.mails', ['email' => $agentportal['email'], 'password' => $randum_pasword], function ($message) use ($request) {
                     $message->to($request->email);
@@ -106,7 +106,7 @@ class AgentsController extends Controller
 
     public function resend_email_agency(Request $request){
         $resend_data=AgencyPortal::where('agency_id',$request->id)->first();
-        $password=decrypt($resend_data->password);
+        $password=$resend_data->password;
         $email=$resend_data->email;
         Mail::send('agency.modules.agent.mail.mails', ['email'=> $email , 'password' => $password], function($message) use($email){
             $message->to($email);
